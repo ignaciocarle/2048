0b34d93ef112dc3f659c143effb622cfc5167d3e
ignaciocarle <ignaciocarle@gmail.com>
2022-03-25 20:21:52 -0300

    move right y move left listos

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { map } from 'rxjs';
+
 
 @Component({
   selector: 'app-board',
@@ -9,7 +9,8 @@ import { map } from 'rxjs';
 export class BoardComponent implements OnInit {
 
   boardSize: number = 4;
-  board: number[] = Array(this.boardSize * this.boardSize).fill(0);
+  board!: number[][];
+  previousBoard!: number[][];
   goal: number = 2048;
   score: number = 0;
 
@@ -20,41 +21,102 @@ export class BoardComponent implements OnInit {
   }
 
   newgame(size: number) {
-    this.board = [];
+    const newBoard: number[][] = Array(this.boardSize).fill(Array(this.boardSize).fill(0));
 
     const testBoard = [
-      2, 0, 0, 2,
-      0, 2, 2, 4,
-      0, 0, 2, 2,
-      0, 2, 4, 2
+      [2, 0, 0, 2],
+      [0, 2, 4, 2],
+      [0, 2, 2, 2],
+      [0, 2, 0, 0]
     ]
-    testBoard.forEach(e => {
+    this.board = testBoard;
+
+
+    /*testBoard.forEach(e => {
       this.board.push(e)
-    });
+    });*/
 
   }
 
   //mover tiene que crear arrays de cada fila y operar cada uno por separado, luego acomodarlos de vuelta como el array orifginal
 
-
-  moveRight() {//arma las filas de izquierda a derecha
-    let newBoard: number[] = []
-    const lines: number[][] = []
-
-
-    this.board.forEach((e, i, a) => {
-      if (i % 4 === 0) {//por cada linea tiene que armarla, filtrarla, sumarla, llenarla y ponerla de vuelta en el array
-        let line = [a[i], a[i + 1], a[i + 2], a[i + 3]];
-        console.log(line);
-        const filtered: number[] = line.filter(e => e);
-        const missing = this.boardSize - filtered.length
-        const zeros = Array(missing).fill(0)
-        const newLine = zeros.concat(filtered)
-        newBoard = newBoard.concat(newLine)
+  collapseRow(row: number[]): number[] {
+    const nextRow: number[] = [];
+    row.forEach((e, i, arr) => {
+      if (arr[i] === 0) return
+      if (arr[i] !== arr[i + 1]) {
+        nextRow.push(arr[i]);
+        return
       }
+      nextRow.push(arr[i] * 2)
+      arr[i + 1] = 0
     });
-    this.board = newBoard
+    return nextRow
   }
+
+  move(board: number[][]): number[][] {
+    const nextBoard: number[][] = [];
+    this.previousBoard = this.board;
+
+    board.forEach((row: number[]) => {
+      const noZeros: number[] = row.filter(e => e !== 0);
+      const collapsed: number[] = this.collapseRow(noZeros);
+      const zeros: number[] = Array(row.length - collapsed.length).fill(0);
+      const newRow: number[] = zeros.concat(collapsed);
+      nextBoard.push(newRow);
+    });
+
+    return nextBoard;
+  }
+
+  reverse(board: number[][]): number[][] {
+    const reversed: number[][] = board;
+    reversed.forEach((row) => {
+      row.reverse()
+    });
+    return reversed;
+  }
+
+  transpose(board: number[][]): number[][] {
+    let transposed: number[][] = Array(this.boardSize).fill(Array(this.boardSize).fill(0));
+    board.forEach((row, x) => {
+      row.forEach((e, y) => {
+        transposed[y][x] = e;
+      });
+    });
+
+
+    console.log(transposed);
+    this.board = transposed
+    return transposed
+  }
+
+  testTranspose() {
+    this.transpose(this.board)
+  }
+
+  moveRight() {
+    this.board = this.move(this.board)
+  }
+
+  moveLeft() {
+    this.board = this.reverse(
+      this.move(
+        this.reverse(
+          this.board)))
+  }
+
+  moveDown() {
+    this.board = this.transpose(
+      this.move(
+        this.transpose(
+          this.board)))
+  }
+
+  moveUp() {
+    this.board = this.transpose(this.move(this.transpose(this.board)))
+  }
+
 
 
 }
